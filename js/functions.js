6bcab8757e4fd6a46be8d2378805f979225505d3
aidan <aidankquinlan@gmail.com>
2019-09-27 18:28:44 -0400

    updates

--- js/functions.js
+++ js/functions.js
@@ -26,7 +26,6 @@ function newPhrase() {
   ];
 }
 
-
 // only loads once the window completely loads
 window.onload = function() {
   console.log(':-)');
@@ -44,45 +43,45 @@ function timer() {
 
 // RANDOM LIST
 // ---------------------------------------------
-// var random = document.querySelector('.random');
-// for (var i = random.children.length; i >= 0; i--) {
-//     random.appendChild(random.children[Math.random() * i | 0]);
-// }
+var random = document.querySelector('.random');
+for (var i = random.children.length; i >= 0; i--) {
+    random.appendChild(random.children[Math.random() * i | 0]);
+}
 
 // ADVANCE SLIDES
 // ---------------------------------------------
-// var slideIndex = 1;
-// showDivs(slideIndex);
-//
-// function plusDivs(n) {
-//   showDivs(slideIndex += n);
-// }
-//
-// function showDivs(n) {
-//   var i;
-//   var x = document.getElementsByClassName("slide");
-//   if (n > x.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = x.length} ;
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.opacity = "0";
-//     x[i].style.zIndex = "0";
-//   }
-//   x[slideIndex-1].style.opacity = "1";
-//   x[slideIndex-1].style.zIndex = "999";
-// }
-//
-// document.onkeydown = function(e) {
-//     switch (e.keyCode) {
-//         case 37:
-//             //left
-//             plusDivs(-1);
-//             break;
-//         case 39:
-//             //right
-//             plusDivs(+1);
-//             break;
-//     }
-// };
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slide");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.opacity = "0";
+    x[i].style.zIndex = "0";
+  }
+  x[slideIndex-1].style.opacity = "1";
+  x[slideIndex-1].style.zIndex = "999";
+}
+
+document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 37:
+            //left
+            plusDivs(-1);
+            break;
+        case 39:
+            //right
+            plusDivs(+1);
+            break;
+    }
+};
 
 function toggleAbout() {
   var about = document.querySelector(".about");
